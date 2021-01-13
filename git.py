355b9a5f1374fc93fa2684a7b7a60a4d193210dc
JoeOlafs <jaolafsson@hotmail.com>
2021-01-13 23:32:57 +0200

    Added comments on git.py

--- git.py
+++ git.py
@@ -3,8 +3,8 @@ import os
 from github import Github
 
 folder = str(sys.argv[1])
-path = os.environ.get('Progs')
-token = os.environ.get('GitToken')
+path = os.environ.get('Progs')      #Add project directory to env, Name it 'Progs' or change here
+token = os.environ.get('GitToken')  #Add Github token to env, Name it 'GitToken' or change it here
 _dir = path + '/' + folder
 
 g=Github(token)
@@ -12,6 +12,7 @@ user = g.get_user()
 login = user.login
 repo = user.create_repo(folder)
 
+#Commands to create files and perform inital commit
 commands = [f'echo "# {repo.name}" >> README.md',
 			f'echo "#{repo.name}" >> main.{sys.argv[2]}',
             'git init',
@@ -20,6 +21,7 @@ commands = [f'echo "# {repo.name}" >> README.md',
             'git commit -m "Initial commit"',
             'git push -u origin master']
 
+#Create and navigate to correct directory
 if sys.argv[2] == "py" or sys.argv[2] == "java":
     os.mkdir(_dir)
     os.chdir(_dir)
@@ -27,8 +29,7 @@ if sys.argv[2] == "py" or sys.argv[2] == "java":
     for c in commands:
         os.system(c)
 
-    #os.system(f'echo "#{repo.name}" >> main.py')
     os.system('code .')
 
 else:
-    print("create <fldername>")
+    print("create <fldername>"
